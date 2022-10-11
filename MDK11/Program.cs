d3761ece1cd30b87b4ed3874320edee7ecde48d0
annse <annse@LITE>
2022-10-11 22:18:01 +0300

    Добавьте файлы проекта.

---
+++ MDK11/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MDK11
+{//найти номера столбцов произведение элементов которого положительное
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string pos;
+            Random random = new Random();
+            int[,] _matr = new int[10, 10];
+            _matr = Creat(ref _matr, random);
+            DoMatr(_matr, out pos);
+            WriteMatr(_matr);
+            Console.Write(pos);
+            Console.ReadKey();
+        }
+        private static int [,] Creat(ref int [,] matr, Random random)
+        {
+            for (int i = 0; i < matr.GetLength(0); i++)
+            {
+                for(int j = 0; j < matr.GetLength(1); j++)
+                {
+                    matr[i, j] = random.Next(-9,9); ;
+                }
+            } return matr;
+        }
+
+        private static void WriteMatr(int[,] matr)
+        {
+            for (int i = 0; i < matr.GetLength(0); i++)
+            {
+                for (int j = 0; j < matr.GetLength(1); j++)
+                {
+                    Console.Write("{0,3}",matr[i,j]);
+                }
+                Console.WriteLine();
+            }
+            return;
+        }
+
+        private static void DoMatr(int[,] matr, out string pos)
+        {
+            int kol;
+            pos = "";
+            for (int j = 0; j < matr.GetLength(0); j++)
+            {
+                kol = 0;
+                for (int i = 0; i < matr.GetLength(1); i++)
+                {
+                    if (matr[i,j] < 0)
+                    {
+                        kol++;
+                    }
+                }
+                if (kol % 2 == 0)
+                {
+                    pos += $"{j+1};";
+                }
+            }
+        }
+    }
+}
---
+++ MDK11/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("MDK11")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MDK11")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("33153e04-5964-465b-83a0-be90b83b1f9f")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
